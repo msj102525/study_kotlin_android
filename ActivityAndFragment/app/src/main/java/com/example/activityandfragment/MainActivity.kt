b307dcc2f3e0852bfffbf20247f356603b200b9e
DoorWinBell <msj102525@gmail.com>
2025-01-23 21:50:10 +0900

    안드로이드 기본 요소 20250123

---
+++ ActivityAndFragment/app/src/main/java/com/example/activityandfragment/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.activityandfragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 버튼 셋팅
+        settingButtons()
+    }
+
+    private fun settingButtons() {
+        val button = findViewById<Button>(R.id.button);
+        button.setOnClickListener {
+            // 버튼을 클릭했을 때 처리
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+}
